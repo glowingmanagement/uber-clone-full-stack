b5ff95112d9c46b340047411c51328240a9c8d99
Josh Holmes <josh@glowingmanagement.com>
2022-07-30 19:00:08 +0100

    refactor

---
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/Header.js
@@ -0,0 +1,25 @@
+import { View, Text, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import styles from "./styles";
+
+const RestaurantHeader = ({ restaurant }) => {
+  return (
+    <View style={styles.page}>
+      <Image source={{ uri: restaurant.image }} style={styles.image} />
+      <LinearGradient
+        colors={["rgba(0,0,0,0.8)", "transparent"]}
+        style={styles.background}
+      ></LinearGradient>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>{restaurant.name}</Text>
+        <Text style={styles.subtitle}>
+          £{restaurant.deliveryFee}0 &#8226; {restaurant.minDeliveryTime}-
+          {restaurant.maxDeliveryTime} minutes
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default RestaurantHeader;
--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,6 +1,9 @@
 import { View, Text, Image, FlatList, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
+import DishListItem from "../../components/DishListItem";
+import Header from "./Header";
+import styles from "./styles";
 
 import restaurants from "../../../data/restaurants.json";
 const restaurant = restaurants[0];
@@ -8,12 +11,6 @@ const restaurant = restaurants[0];
 const RestaurantDetailsScreen = () => {
   return (
     <View style={styles.page}>
-      <Image source={{ uri: restaurant.image }} style={styles.image} />
-      <LinearGradient
-        colors={["rgba(0,0,0,0.8)", "transparent"]}
-        style={styles.background}
-      ></LinearGradient>
-
       <Ionicons
         name="arrow-back-circle"
         size={50}
@@ -21,49 +18,14 @@ const RestaurantDetailsScreen = () => {
         style={styles.iconContainer}
       />
 
-      <View style={styles.container}>
-        <Text style={styles.title}>{restaurant.name}</Text>
-        <Text style={styles.subtitle}>
-          £{restaurant.deliveryFee}0 &#8226; {restaurant.minDeliveryTime}-
-          {restaurant.maxDeliveryTime} minutes
-        </Text>
-      </View>
+      <FlatList
+        ListHeaderComponent={() => <Header restaurant={restaurant} />}
+        showsVerticalScrollIndicator={false}
+        data={restaurant.dishes}
+        renderItem={({ item }) => <DishListItem dish={item} />}
+      />
     </View>
   );
 };
 
 export default RestaurantDetailsScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    margin: 10,
-  },
-  iconContainer: {
-    position: "absolute",
-    top: 50,
-    left: 15,
-  },
-  page: {
-    flex: 1,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 5 / 3,
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  subtitle: {
-    color: "#525252",
-    fontSize: 15,
-  },
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 200,
-  },
-});
