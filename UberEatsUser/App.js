12b46d2dee9ed998ce36119786a48e13783a826b
Josh Holmes <josh@glowingmanagement.com>
2022-07-30 19:27:21 +0100

    plus and minus functions

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantDetailsScreen from "./src/screens/RestaurantDetailsScreen";
+import DishDetailsScreen from "./src/screens/DishDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantDetailsScreen />
+      <DishDetailsScreen />
       <StatusBar style="light" />
     </View>
   );
---
+++ UberEatsUser/src/screens/DishDetailsScreen/index.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import restaurants from "../../../data/restaurants.json";
+
+const dish = restaurants[0].dishes[0];
+
+const DishDetailsScreen = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const onMinus = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const onPlus = () => {
+    setQuantity(quantity + 1);
+  };
+
+  return (
+    <View style={styles.page}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <View style={styles.separator}></View>
+      <View style={styles.row}>
+        <AntDesign
+          name="minuscircleo"
+          size={60}
+          color={"black"}
+          onPress={onMinus}
+        />
+        <Text style={styles.quantity}>{quantity}</Text>
+        <AntDesign
+          name="pluscircleo"
+          size={60}
+          color={"black"}
+          onPress={onPlus}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default DishDetailsScreen;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    width: "100%",
+    paddingVertical: 50,
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "700",
+    marginVertical: 10,
+  },
+  description: {
+    color: "#696969",
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "lightgrey",
+    marginVertical: 10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  quantity: {
+    fontSize: 25,
+    marginHorizontal: 20,
+  },
+});
