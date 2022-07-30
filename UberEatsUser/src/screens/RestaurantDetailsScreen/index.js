d651bb73df89aa824a5877d15df9170d3dc5c654
Josh Holmes <josh@glowingmanagement.com>
2022-07-30 18:11:59 +0100

    add back button

--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,4 +1,5 @@
 import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import restaurants from "../../../data/restaurants.json";
 const restaurant = restaurants[0];
@@ -7,11 +8,21 @@ const RestaurantDetailsScreen = () => {
   return (
     <View style={styles.page}>
       <Image source={{ uri: restaurant.image }} style={styles.image} />
-      <Text style={StyleSheet.title}>{restaurant.name}</Text>
-      <Text style={styles.subtitle}>
-        £{restaurant.deliveryFee}0 &#8226; {restaurant.minDeliveryTime}-
-        {restaurant.maxDeliveryTime} minutes
-      </Text>
+
+      <Ionicons
+        name="arrow-back-circle"
+        size={50}
+        color="white"
+        style={styles.iconContainer}
+      />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>{restaurant.name}</Text>
+        <Text style={styles.subtitle}>
+          £{restaurant.deliveryFee}0 &#8226; {restaurant.minDeliveryTime}-
+          {restaurant.maxDeliveryTime} minutes
+        </Text>
+      </View>
     </View>
   );
 };
@@ -19,11 +30,28 @@ const RestaurantDetailsScreen = () => {
 export default RestaurantDetailsScreen;
 
 const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  iconContainer: {
+    position: "absolute",
+    top: 50,
+    left: 15,
+  },
   page: {
     flex: 1,
   },
   image: {
     width: "100%",
     aspectRatio: 5 / 3,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  subtitle: {
+    color: "#525252",
+    fontSize: 15,
   },
 });
