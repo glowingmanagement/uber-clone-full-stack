89e2544a1ba1082ed7f460e1c7f5478e473289b5
Josh Holmes <josh@glowingmanagement.com>
2022-07-30 09:31:53 +0100

    render image and add data

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Restaurant Item */}
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg",
+          }}
+          style={styles.image}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3,
+  },
+  restaurantContainer: {
+    width: "100%",
   },
 });
